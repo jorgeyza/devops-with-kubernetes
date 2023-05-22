efa17ca5e005adb864936aa3009198d955398526
Jorge Eyzaguirre <jorgeyza@gmail.com>
2023-05-22 16:46:16 -0500

    Add function to generate random string with timestamp

---
+++ index.ts
@@ -0,0 +1,17 @@
+function generateRandomStringWithTimestamp() {
+  const timestamp = new Date().toISOString();
+  const randomUuid = generateRandomUuid();
+
+  console.log(`${timestamp}: ${randomUuid}`);
+  setTimeout(() => generateRandomStringWithTimestamp(), 5000);
+}
+
+function generateRandomUuid() {
+  return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function (c) {
+    const r = (Math.random() * 16) | 0,
+      v = c === "x" ? r : (r & 0x3) | 0x8;
+    return v.toString(16);
+  });
+}
+
+generateRandomStringWithTimestamp();
